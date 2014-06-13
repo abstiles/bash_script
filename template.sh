a30048d35aff1346023cd8e44fe22a3f36bfe884
Andrew Stiles <andrewbstiles@gmail.com>
2014-06-13 08:41:36 -0500

    Initial commit

---
+++ template.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# The following options enable a kind of strict mode: exit script when a
+# command fails, and fail when accessing undefined variables.
+set -eu
+set -o pipefail
+
+# Allow setting via environment, falling back to a default
+readonly EXAMPLE=${EXAMPLE:-"default"}
+
+readonly USAGE="USAGE:
+$0 [<options>] [--] [<positional arg> ...]
+$0 --help
+
+A template for a Bash script. In the interest of readability, this makes no
+attempt to be POSIX compliant, and therefore Bash-only syntax is used
+liberally.
+
+Options:
+    --example, -x <val>  Sets the example variable to <val>.
+                             DEFAULT: \"default\"
+    --help, -h           Prints this usage information and exits.
+"
+
+function usage {
+	echo "$USAGE"
+}
+
+# For convenience, you can pipe to STDERR for error output
+function STDERR {
+	cat >&2
+}
+
+# Process options, filter out positional arguments
+declare -a positional_args
+while (( $# )); do
+	case $1 in
+		--help|-h)    usage; exit ;;
+		--example|-x) example_opt="$2"; shift ;;
+		--example=*)  example_opt=${1/--example=/} ;;
+		--) # Remaining args are to be treated as positional
+			shift
+			# Consume the rest of the arguments
+			while (( $# )); do positional_args+=("$1"); done
+			;;
+		-*) STDERR <<< "ERROR: Unrecognized option"; exit 1 ;;
+		*) positional_args+=("$1") ;;
+	esac
+	shift
+done
+# Handle the positional arguments
+if (( ${#positional_args[@]} > 0 )); then
+	set -- "${positional_args[@]}"
+	unset positional_args # Don't pollute the global namespace
+fi
+
+function main {
+	# Use the value set by the options, falling back to the global variable
+	local example_var=${example_opt:-$EXAMPLE}
+	echo "Example var: $example_var"
+
+	local i=0
+	echo "Positional args:"
+	for arg in "$@"; do
+		i=$((i + 1))
+		echo "    $i) $arg"
+	done
+}
+
+main "$@"
